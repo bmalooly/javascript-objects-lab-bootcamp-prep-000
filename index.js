dd46efa93e9bd26d2e68d5dcdadd7d10ec19ca7d
Brittany Malooly <bmalooly@gmail.com>
2019-01-07 03:35:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {
   'lunch': 'taquitos',
   'dinner': 'enchiladas'
 }
-function updateObjectWithKeyAndValue(object, key, value): {
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]:value})
 }
