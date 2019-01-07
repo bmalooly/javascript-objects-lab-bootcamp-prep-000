0e6f72e09ab10e3c0be0adb514aa33c12d5a6c3a
Brittany Malooly <bmalooly@gmail.com>
2019-01-07 03:42:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  
+  delete object[key]
+  return object
+}
